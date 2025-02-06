c1829aef6564e5e6e08a3a0fc95dabece5d9d94a
Marty Fierbaugh <mfierbau@cisco.com>
2025-02-06 13:17:13 -0500

    modified

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 # Update script for clab
 
-clab version upgrade
+sudo clab version upgrade
 # Set SUID bit on Containerlab binary
-chmod u+s `which containerlab`
+sudo chmod u+s `which containerlab`
 # Create clab_admins Unix group
-groupadd -r clab_admins
+sudo groupadd -r clab_admins
 # Add current user to clab_admins group
-usermod -aG clab_admins admin
+sudo usermod -aG clab_admins admin
